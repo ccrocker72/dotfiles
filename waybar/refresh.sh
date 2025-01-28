0c7b3ea9475bf34d79d72d924d3de0d92cf65298
Christopher Crocker <ccrocker7227@gmail.com>
2025-01-28 08:52:43 -0500

    waybar refresh shortcut added

--- waybar/refresh.sh
+++ waybar/refresh.sh
@@ -4,7 +4,8 @@
 if pgrep -x "waybar" > /dev/null; then
     # If running, kill the waybar process
     pkill -x "waybar"
-else
+fi
+if ! (pgrep -x "waybar"); then
     # If not running, start waybar
     waybar &
 fi
